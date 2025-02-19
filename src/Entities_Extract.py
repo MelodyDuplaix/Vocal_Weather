ec47ad4bca11c5253f403782db55017b653d38bf
MelodyDuplaix <melo.surseine@gmail.com>
2025-02-19 16:30:37 +0100

    Refactor: séparation du chargement du modèle  d'extraction d'entité pour accélérer les traitement

--- src/Entities_Extract.py
+++ src/Entities_Extract.py
@@ -1,8 +1,21 @@
 from transformers import CamembertTokenizer, AutoModelForTokenClassification, pipeline
 import dateparser
 from transformers.pipelines.base import Pipeline
+from datetime import datetime, timedelta
 
-def extract_entities(text):
+def load_model():
+    """
+    Load the Camembert model fine-tuned for Named Entity Recognition (NER) with dates and localisations.
+
+    Returns:
+        tuple: A tuple containing the tokenizer and the model.
+    """
+    tokenizer = CamembertTokenizer.from_pretrained("Jean-Baptiste/camembert-ner-with-dates")
+    model = AutoModelForTokenClassification.from_pretrained("Jean-Baptiste/camembert-ner-with-dates")
+    ner_pipeline: Pipeline = pipeline("ner", model=model, tokenizer=tokenizer, aggregation_strategy="simple")
+    return ner_pipeline
+
+def extract_entities(text, ner_pipeline):
     """
     Extract entities from a given text using a Camembert model fine-tuned for Named Entity Recognition (NER) with dates and localisations.
 
@@ -12,13 +25,8 @@ def extract_entities(text):
     Returns:
         dict: A dictionary containing the extracted dates and localisations.
     """
-    # Load the model and tokenizer and create the pipeline
-    tokenizer = CamembertTokenizer.from_pretrained("Jean-Baptiste/camembert-ner-with-dates")
-    model = AutoModelForTokenClassification.from_pretrained("Jean-Baptiste/camembert-ner-with-dates")
-    nlp: Pipeline = pipeline("ner", model=model, tokenizer=tokenizer, aggregation_strategy="simple")
-    
     # Extract entities
-    entities = nlp(text)
+    entities = ner_pipeline(text)
     
     # Extract dates and localisations from entities
     dates = []
@@ -27,7 +35,10 @@ def extract_entities(text):
     for entite in entities:
         if entite["entity_group"] == "DATE":
             date_obj = dateparser.parse(entite["word"], languages=["fr"])
-            dates.append(date_obj)
+            if date_obj is None:
+                dates.append(entite["word"])
+            else:
+                dates.append(date_obj)
         elif entite["entity_group"] == "LOC":
             localisations.append(entite["word"])
             
@@ -38,6 +49,8 @@ def extract_entities(text):
 
 if __name__ == "__main__":
     text = input("Entrez le texte à analyser: ")
-    entities = extract_entities(text)
-    print(f"Dates: {entities['date']}")
+    ner_pipeline = load_model()
+    entities = extract_entities(text, ner_pipeline)
+    dates = [date.strftime('%Y-%m-%d %H:%M:%S') if isinstance(date, datetime) else date for date in entities['date']]
+    print(f"Dates: {dates}")
     print(f"Localisations: {entities['localisation']}")
