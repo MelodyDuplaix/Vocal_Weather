accb15637761cac15a135538508fca79ce6e7c88
MelodyDuplaix <melo.surseine@gmail.com>
2025-02-21 11:33:38 +0100

    Feat: Création du de la structure du frontend en Next.js avec des fichiers et une structure de configuration

---
+++ frontend/vocal_weather/src/app/page.js
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <ol>
+          <li>
+            Get started by editing <code>src/app/page.js</code>.
+          </li>
+          <li>Save and see your changes instantly.</li>
+        </ol>
+        <div>
+          <p>Ceci est un test pour voir.</p>
+        </div>
+      </main>
+    </div>
+  );
+}
